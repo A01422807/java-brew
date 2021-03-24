0aaa117f4fbb20e22fdb2cf827662af9bd91ff66
salvasgdo <A01421806@itesm.mx>
2021-03-23 23:55:43 -0600

    Primer acercamiento a react

---
+++ src/components/Team.js
@@ -0,0 +1,30 @@
+export const Team = (props) => {
+  return (
+    <div id='team' className='text-center'>
+      <div className='container'>
+        <div className='col-md-8 col-md-offset-2 section-title'>
+          <h2>Conoce el Equipo</h2>
+          <p>
+          Las partes involucradas son personas que participan activamente en un proyecto, que influyen en el resultado del proyecto. Los perfiles de las partes involucradas son:
+          </p>
+        </div>
+        <div id='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-3 col-sm-6 team'>
+                  <div className='thumbnail'>
+                    {' '}
+                    <img src={d.img} alt='...' className='team-img' />
+                    <div className='caption'>
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
