2a3af908386bdfd9038cfaa8f2bebff964fe810a
peredo98 <36453392+peredo98@users.noreply.github.com>
2021-06-12 22:03:32 -0500

    apollo testing fix

--- src/__test__/coffeeList.test.js
+++ src/__test__/coffeeList.test.js
@@ -1,6 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import CoffeeList from '../components/coffeeList';
+// 1
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+import { baseURL } from '../config';
 
 test('renders CoffeeList', () => {
-  render(<CoffeeList />);
+  
+
+  // 2
+  let httpLink = createHttpLink({
+    uri: baseURL+'/graphql/'
+  });
+
+  // 3
+  let client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+  });
+  render(
+    <ApolloProvider client={client}>
+      <CoffeeList />
+    </ApolloProvider>
+    );
 });
